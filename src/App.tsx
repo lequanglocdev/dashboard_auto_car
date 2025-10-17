234b6309fa433969ae3769f660ce09b3b3c1121c
lequanglocdev <quanglocdev@gmail.com>
2025-10-17 22:50:31 +0700

    router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
-import { Button } from "@/components/ui/button";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./routes";
 
 function App() {
-  return (
-    <div >
-      <Button>Click me</Button>
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
